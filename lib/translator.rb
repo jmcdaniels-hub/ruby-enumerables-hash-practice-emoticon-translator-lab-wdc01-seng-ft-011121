5572f3045d325d9f9d29493472b1298ce4bfbf45
Jade McDaniels <jademcdaniels81@gmail.com>
2021-01-04 15:49:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,7 @@ def load_library(file_path)
  emoticon_library = YAML.load_file(file_path)
  emoticon_hash = {}
  emoticon_library.each do |meaning, graphic|
-   emoticon_hash[meaning] = {english: value[0], japanese:[1]}
+   emoticon_hash[meaning] = {:english => value[0], japanese:[1]}
   end
  emoticon_hash
 end
@@ -19,21 +19,5 @@ def get_japanese_emoticon
 end
 
 def get_english_meaning #(file_path, emoticon)
- #path = load_library(file_path)
  
- #result = load_library(file_path,emoticon)
- #result ? result : "Sorry, that emoticon was not found"
- 
- 
- 
- 
- 
- #result = path[meaning][:japanese][emoticon]
-
-  #if path[meaning][:japanese][emoticon] == nil 
-    
-    #result = "Sorry, that emoticon was not found"
-  
-  #end
- #result
 end
